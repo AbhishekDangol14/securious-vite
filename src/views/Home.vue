9d5f04a1d9d48989a8291b3f2b8ce9db856be74d
Aryal <rish.aryal@gmail.com>
2021-12-21 14:30:41 +0545

    Added Vue template with tailwind CSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="flex justify-center pt-8">
+      <div>
+        <img src="../assets/logo.svg" alt="Securious" class="h-48 w-96"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style>
+.home {
+  background: url("https://app.securious.de/images/common/page-background.png") no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
+</style>
