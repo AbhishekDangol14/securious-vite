81d9b26479d02069951823a5d8c319f5fc286a0f
Aryal <rish.aryal@gmail.com>
2021-12-22 14:15:26 +0545

    fix(UI): Fixed UI styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,34 @@
 <template>
-  <div class="home overflow-hidden">
-    <div class="flex flex-row justify-center pt-48">
-        <router-link to="/"><img src="../assets/logo.svg" alt="Securious" class="h-4/5 w-80"/></router-link>
-    </div>
-    <div class="label-text text-white text-center text-xl tracking-widest mt-16">WILLKOMMEN</div>
-    <div class="flex justify-center"><div class="note-text text-white text-center text-sm mt-4 max-w-sm">Loggen Sie sich ein oder registrieren Sie sich jetzt, um Ihre Unternehmenssicherheit zu prüfen und zu verbessern! </div></div>
-    <div class="btn-container flex justify-center space-x-8 mt-8">
-      <button class="btn border rounded-full text-center font-serif text-sm text-white p-1 w-48"><router-link to="/">REGISTRIEREN</router-link></button>
-      <button class="btn border rounded-full text-center font-serif text-sm text-white p-1 w-48"><router-link to="/">EINLOGGEN</router-link></button>
+  <div class="flex flex-col justify-center align-middle home">
+    <div class="grid justify-items-center">
+      <div>
+        <router-link to="/"><img src="../assets/logo.svg" alt="Securious" class="h-24"/></router-link>
+      </div>
+      <div class="text-white text-2xl tracking-widest mt-16">WILLKOMMEN</div>
+      <div class="note-text text-white text-center text-sm mt-4">
+        Loggen Sie sich ein oder registrieren Sie sich jetzt, um Ihre Unternehmenssicherheit zu prüfen und zu verbessern!
+      </div>
+      <div class="mt-8 flex space-x-4">
+        <router-link class="border rounded-full text-center  text-sm text-white p-2 w-96" to="/register">REGISTRIEREN</router-link>
+        <router-link class="border rounded-full text-center text-sm text-white p-2 w-96" to="/login">EINLOGGEN</router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
-  components: {
-  },
-})
+import { Vue } from 'vue-class-component';
 export default class Home extends Vue {}
 </script>
 
-<style>
+<style scoped>
 .home {
   background: url("https://app.securious.de/images/common/page-background.png") no-repeat;
   background-size: cover;
-  height: 100vh;
-  max-height: 100%;
-}
-.label-text{
-  font-family: 'Titillium Web', sans-serif;
-}
-.note-text{
-  font-family: 'Titillium Web', sans-serif;
+  min-height: 100vh;
 }
 
-.btn:hover {
+button:hover {
   background-color: #fff;
   color: #1269b8;
 }
