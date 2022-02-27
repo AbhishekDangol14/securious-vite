6fbb982023611f7556d71aafea02661f6fb54b47
subhamghimire <subham.ghimire26@gmail.com>
2022-02-27 13:27:39 +0545

    Sidebar need fix

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,30 +1,26 @@
 <template>
-  <div class="sidebar bg-white shadow-sidebar-shadow h-screen">
-    <div class="logo pt-4 w-56 pl-3 pb-3">
+  <div class="sidebar bg-white shadow-secondary h-screen" v-if="menu">
+    <div class="logo p-3">
       <img src="../assets/views/securious-logo.svg" />
     </div>
-    <div class="border-b border-grey-grey border-opacity-20 my-6"></div>
-    <Menu v-for="item in menu.menus" :item="item" v-bind:key="item.label" />
-    <div v-if="menu.role[0].role == 'Admin'">
-      <div class="border-b border-grey-grey border-opacity-20 pt-4"></div>
-
-      <div class="pt-4 pl-6 flex-cols space-y-4">
-        <div class="flex text-grey-grey space-x-2 pb-3 text-base">
+    <div class="border-line pt-4"><hr /></div>
+    <Menu v-for="item in menu" :item="item" v-bind:key="item" />
+    <div v-if="role == 'Admin'">
+      <hr />
+      <div class="pt-4 pl-4 flex-cols space-y-4">
+        <div class="flex space-x-2 text-sm pb-3">
           <img src="../assets/components/user.svg" /><router-link
             :to="{ name: 'users' }"
             >Users</router-link
           >
         </div>
-        <div class="flex text-grey-grey space-x-2 text-base pb-3">
+        <div class="flex space-x-2 text-sm pb-3">
           <img src="../assets/components/setting.svg" /><router-link
             :to="{ name: 'settings' }"
             >Setting</router-link
           >
         </div>
-        <button
-          class="flex text-grey-grey space-x-2 text-base pb-3"
-          @click="logout"
-        >
+        <button class="flex space-x-2 text-sm pb-3" @click="logout">
           <img src="../assets/components/logout.svg" />
           <div>Logout</div>
         </button>
@@ -41,6 +37,43 @@
       </div>
     </div>
   </div>
+  <div class="border-b border-grey-grey border-opacity-20 my-6"></div>
+  <Menu v-for="item in menu.menus" :item="item" v-bind:key="item.label" />
+  <div v-if="menu.role[0].role == 'Admin'">
+    <div class="border-b border-grey-grey border-opacity-20 pt-4"></div>
+
+    <div class="pt-4 pl-6 flex-cols space-y-4">
+      <div class="flex text-grey-grey space-x-2 pb-3 text-base">
+        <img src="../assets/components/user.svg" /><router-link
+          :to="{ name: 'users' }"
+          >Users</router-link
+        >
+      </div>
+      <div class="flex text-grey-grey space-x-2 text-base pb-3">
+        <img src="../assets/components/setting.svg" /><router-link
+          :to="{ name: 'settings' }"
+          >Setting</router-link
+        >
+      </div>
+      <button
+        class="flex text-grey-grey space-x-2 text-base pb-3"
+        @click="logout"
+      >
+        <img src="../assets/components/logout.svg" />
+        <div>Logout</div>
+      </button>
+    </div>
+  </div>
+  <div v-show="token">
+    <hr />
+    <div
+      class="cursor-pointer flex space-x-2 pt-4 pl-4"
+      @click="stopImpersonating"
+    >
+      <i class="fa fa-stop-circle" aria-hidden="true"></i>
+      <div class="text-sm font-normal">Stop Impersonating</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -51,46 +84,38 @@ import { useStore } from "vuex";
 import Menu from "../components/Menu.vue";
 
 export default defineComponent({
-    components: {
-        Menu
-    },
-    setup() {
-        const store = useStore()
-        let user = localStorage.getItem("USER")
-        let menu = ref([])
-        let role = ref()
+  components: {
+    Menu,
+  },
+  setup() {
+    const store = useStore();
+    let user = localStorage.getItem("USER");
+    let menu = ref([]);
+    let role = ref();
 
-        const token = localStorage.getItem('IMPERSONATOR_TOKEN')
+    const token = localStorage.getItem("IMPERSONATOR_TOKEN");
 
-        if (user){
-        menu.value = JSON.parse(user).menus
-        role.value = JSON.parse(user).role[0].role
-        }
+    if (user) {
+      menu.value = JSON.parse(user).menus;
+      role.value = JSON.parse(user).role[0].role;
+    }
 
     function logout() {
       store.dispatch(LOGOUT);
     }
 
-        function stopImpersonating() {
-            const user = localStorage.getItem('IMPERSONATOR_USER')
-            token ? localStorage.setItem('ID_TOKEN_KEY',token) : null
-            user ? localStorage.setItem('USER',user) : null
-            localStorage.removeItem('IMPERSONATOR_TOKEN')
-            localStorage.removeItem('IMPERSONATOR_USER')
-            router.push({ name: 'users' })
-        }
-        
-        return {
-            menu,
-            role,
-            logout,
-            stopImpersonating,
-            token
-        }
+    function stopImpersonating() {
+      const user = localStorage.getItem("IMPERSONATOR_USER");
+      token ? localStorage.setItem("ID_TOKEN_KEY", token) : null;
+      user ? localStorage.setItem("USER", user) : null;
+      localStorage.removeItem("IMPERSONATOR_TOKEN");
+      localStorage.removeItem("IMPERSONATOR_USER");
+      router.push({ name: "users" });
     }
 
     return {
       menu,
+      role,
       logout,
       stopImpersonating,
       token,
